1bb98f5f4f9926417085403eafba24cbabadb35c
Soikat1139 <soikatabd@gmail.com>
2023-01-01 18:46:52 +0600

    Completed

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
+const {hash}=window.location;
+
+const message=atob(hash.replace('#',''));
+
+if(message){
+    document.querySelector('#message-form').classList.add('hide');
+    document.querySelector('#message-show').classList.remove('hide');
+    document.querySelector('h1').innerHTML=message;
+}
+
 document.querySelector('form').addEventListener('submit',event=>{
     event.preventDefault();
+    const message=document.querySelector('#message-form');
+    const link=document.querySelector('#link-form');
     const input=document.querySelector('#message-input');
     const encrypted=btoa(input.value);
     const input2=document.querySelector('#link-input');
 
     input2.value=`${window.location}#${encrypted}`;
 
+    message.classList.add('hide');
+    link.classList.remove('hide')
     input2.select();
 
 });
